450a0357f438affa2c5c94b50b2cf2c4fc8c16b3
Matt Deal <contact@mattdeal.com.au>
2024-04-08 15:09:12 +1000

    first upload

--- web/README.md
+++ web/README.md
@@ -1,36 +1,105 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
-
 ## Getting Started
 
-First, run the development server:
+First, install the dependencies:
 
 ```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
+cd api
+pnpm install
 ```
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+```bash
+cd web
+pnpm install
+```
 
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
+you'll need to setup a Postgres DB, you can do this locally just to test.
 
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
+```bash
+createdb <db_name>
+```
 
-## Learn More
+You'll have to add two tables to your db
 
-To learn more about Next.js, take a look at the following resources:
+a `users` and a `posts` tables
 
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
+The commands for these are as follows
 
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
+`users`:
 
-## Deploy on Vercel
+```sql
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+```
 
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
+`posts`:
 
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+```sql
+CREATE TABLE posts (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    sub_title VARCHAR(255),
+    category: TEXT,
+    type: TEXT,
+    user_id INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+```
+
+Then you'll need to create a `.env` file in the `api` directory with the following content:
+
+```bash
+DATABASE_URL=postgres://<username>:<password>@localhost:5432/<db_name>
+```
+
+you will also have to add a JWT_SECRET to the `.env` file
+
+Since this is just local you can just do something like
+
+`JWT_SECRET='<Some String>'`
+
+You will then need to add this some `JWT_SECRET` to the `.env` file in the `web` directory
+
+Lastly, you need to add a `API_URL` to the `.env` file in the `web` directory, this would just be
+localhost:3000 for local development.
+
+once that is completed you can then start the server and app
+
+To run the project:
+
+`cd api`
+
+```bash
+pnpm dev
+```
+
+Open a new terminal and run the following command
+
+`cd web`
+
+```bash
+pnpm dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result of the web app
+
+From here, you can register an account, and then login. Once you're logged in, you can click the admin button to create a new post.
+
+# Accessibility
+
+This project focuses on accessibility. The web app is fully accessible and can be navigated using only a keyboard. The colors are also accessible and the contrast ratio is high enough to be readable. The web app is also responsive and can be used on mobile devices.
+
+I focused on these areas of accessibility:
+
+- Keyboard navigation
+- Color contrast for colorblind users
+- Screen reader support
+- Responsive design for mobile users
+
+Google Light house is currently giving all pages 100% for accessibility.
---
+++ web/src/app/register/page.tsx
@@ -0,0 +1,143 @@
+// 'use client'
+
+// import { z } from 'zod'
+// import { useForm } from 'react-hook-form'
+// import { zodResolver } from '@hookform/resolvers/zod'
+// import { useState } from 'react'
+
+// import Container from '@/components/ui/container'
+// import Section from '@/components/ui/section'
+// import { Button } from '@/components/ui/button'
+// import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
+// import { Input } from '@/components/ui/input'
+// import {
+//   Form,
+//   FormControl,
+//   FormDescription,
+//   FormField,
+//   FormItem,
+//   FormLabel,
+//   FormMessage,
+// } from '@/components/ui/form'
+// import Link from 'next/link'
+// import { useRouter } from 'next/navigation'
+// import { register } from '@/actions'
+// import { Routes } from '@/contstants'
+// import Alert from '@/components/ui/alert'
+// import { RegisterSchema } from '@/zodSchemas'
+
+// export default function Page() {
+//   const [alert, setAlert] = useState({
+//     submitType: false,
+//     text: '',
+//   })
+
+//   const { push } = useRouter()
+
+//   const form = useForm<z.infer<typeof RegisterSchema>>({
+//     resolver: zodResolver(RegisterSchema),
+//     defaultValues: {
+//       email: '',
+//       password: '',
+//       confirm_password: '',
+//     },
+//   })
+
+//   const onSubmit = async (data: z.infer<typeof RegisterSchema>) => {
+//     const { email, password } = data
+
+//     try {
+//       const { message, success } = await register(email, password)
+//       if (!success) {
+//         setAlert({ submitType: success, text: message })
+//         return
+//       }
+
+//       if (success) {
+//         setAlert({ submitType: success, text: message })
+//         setTimeout(() => {
+//           push(Routes.LOGIN)
+//         }, 500)
+//       }
+
+//       if (!success) setAlert({ submitType: success, text: message })
+//     } catch (error) {
+//       setAlert({ submitType: false, text: 'Failed to register.' })
+//     }
+//   }
+
+//   return (
+//     <Section description="Register">
+//       <Container width="2xl" className="mt-24">
+//         <Card className="w-[400px] sm:w-[564px]">
+//           <CardHeader>
+//             <CardTitle>Create an account</CardTitle>
+//           </CardHeader>
+//           <CardContent>
+//             <Form {...form}>
+//               <form onSubmit={form.handleSubmit(onSubmit)}>
+//                 <FormField
+//                   control={form.control}
+//                   name="email"
+//                   render={({ field }) => (
+//                     <FormItem>
+//                       <FormLabel>Email</FormLabel>
+//                       <FormControl>
+//                         <Input id="email" placeholder="Email Address" {...field} />
+//                       </FormControl>
+//                       <FormMessage />
+//                     </FormItem>
+//                   )}
+//                 />
+
+//                 <FormField
+//                   control={form.control}
+//                   name="password"
+//                   render={({ field }) => (
+//                     <FormItem>
+//                       <FormLabel>Password</FormLabel>
+//                       <FormControl>
+//                         <Input type="password" id="password" placeholder="Password" {...field} />
+//                       </FormControl>
+//                       <FormMessage />
+//                     </FormItem>
+//                   )}
+//                 />
+
+//                 <FormField
+//                   control={form.control}
+//                   name="confirm_password"
+//                   render={({ field }) => (
+//                     <FormItem>
+//                       <FormLabel>Confirm Password</FormLabel>
+//                       <FormControl>
+//                         <Input
+//                           type="password"
+//                           id="confirm_password"
+//                           placeholder="Confirm Password"
+//                           {...field}
+//                         />
+//                       </FormControl>
+//                       <FormDescription>This is your public display name.</FormDescription>
+//                       <FormMessage />
+//                     </FormItem>
+//                   )}
+//                 />
+
+//                 <Link href={Routes.LOGIN} className="text-sm font-medium text-blue-600">
+//                   {' '}
+//                   Already have an account? Login
+//                 </Link>
+//                 <CardFooter className="flex justify-end">
+//                   <Button>Register</Button>
+//                 </CardFooter>
+
+//                 {alert.text && <Alert submitType={alert.submitType} text={alert.text} />}
+//               </form>
+//             </Form>
+//           </CardContent>
+//         </Card>
+//       </Container>
+//     </Section>
+//   )
+// }
